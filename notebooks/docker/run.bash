9e869d1fdc5acdf29372e1b71fb302b4e7819b39
Mauricio Jost <mauricio.jost@gmail.com>
2017-03-12 20:36:14 +0100

    Update Dockerfile and run.bash

--- notebooks/docker/run.bash
+++ notebooks/docker/run.bash
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+DATASETS_DIR=`readlink -e ../datasets`
+DATASETS_MOUNTPOINT=/datasets
+
+echo ""
+echo "Zeppelin will be available at:"
+echo "   http://localhost:8080/"
+echo ""
+echo "Can run spark-shell with:"
+echo "   sudo docker ps"
+echo "   sudo docker exec <containerid> spark-shell"
+echo ""
+echo "The datasets will be mounted at:"
+echo "   $DATASETS_MOUNTPOINT"
+echo ""
+echo ""
+
 sudo docker build -t zeppelin .
-DATASETS=`readlink -e ../datasets`
-sudo docker run --volume=$DATASETS:/datasets --rm --net=host -p 8080:8080 zeppelin
+
+sudo docker run --volume=$DATASETS_DIR:$DATASETS_MOUNTPOINT --rm --net=host -p 8080:8080 zeppelin
 
